3873fd45fc2f039a524efdc2a83111b0f0499c57
SERRA Frank <serra!;frank@free.fr>
2018-12-12 17:32:19 +0100

    Initialization du projet

---
+++ datafile-server/src/main/java/org/medipractice/datafileserver/DatafileServerApplication.java
@@ -0,0 +1,14 @@
+package org.medipractice.datafileserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatafileServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatafileServerApplication.class, args);
+	}
+
+}
+
