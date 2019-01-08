8d8c6163013e88134160f2312749b666cd642ab9
SERRA Frank <serra!;frank@free.fr>
2019-01-08 17:19:15 +0100

    Implementation config server

---
+++ config-server/src/main/java/org/medipractice/configserver/ConfigServerApplication.java
@@ -0,0 +1,19 @@
+package org.medipractice.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
+
