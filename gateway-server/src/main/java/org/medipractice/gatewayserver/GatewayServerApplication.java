2da42cd83e4eea4e6beced79082fe6e466efaaaa
SERRA Frank <serra.frank@free.fr>
2019-01-20 14:55:40 +0100

    test de scripts de lancement

--- gateway-server/src/main/java/org/medipractice/gatewayserver/GatewayServerApplication.java
+++ gateway-server/src/main/java/org/medipractice/gatewayserver/GatewayServerApplication.java
@@ -1,9 +1,6 @@
 package org.medipractice.gatewayserver;
 
-import org.medipractice.gatewayserver.model.User;
-import org.medipractice.gatewayserver.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -12,29 +9,11 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @SpringBootApplication
 @EnableZuulProxy
 @EnableDiscoveryClient
-public class GatewayServerApplication implements CommandLineRunner {
+public class GatewayServerApplication {
 
-	@Autowired
-	private UserRepository userRepository;
-
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayServerApplication.class, args);
-	}
-
-	/**
-	 * Callback used to run the bean.
-	 *
-	 * @param args incoming main method arguments
-	 * @throws Exception on error
-	 */
-	@Override
-	public void run(String... args) throws Exception {
-		User user = new User();
-		user.setUsername("ADMIN");
-		user.setPassword("ADMIN");
-
-		//userRepository.save(user);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServerApplication.class, args);
+    }
 
 }
 
