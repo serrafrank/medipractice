44f9d47a5a2aacbaf4dfd527e11ad4c06a935851
SERRA Frank <serra!;frank@free.fr>
2019-01-23 12:52:11 +0100

    Auth server

--- auth-server/src/main/java/org/medipractice/authserver/AuthServerApplication.java
+++ auth-server/src/main/java/org/medipractice/authserver/AuthServerApplication.java
@@ -2,13 +2,7 @@ package org.medipractice.authserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableEurekaClient
