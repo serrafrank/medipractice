faae5ea1a6f9cc0046372800eccf0656bb5d837f
SERRA Frank <frank.serra@versusmind.eu>
2019-05-06 22:58:57 +0200

    bugFix : DROP DATABASE medipractice; ne passe pas avec docker

--- sql/medipractice.sql
+++ sql/medipractice.sql
@@ -17,7 +17,6 @@ SET check_function_bodies = false;
 SET client_min_messages = warning;
 SET row_security = off;
 
-DROP DATABASE medipractice;
 --
 -- TOC entry 2920 (class 1262 OID 16393)
 -- Name: medipractice; Type: DATABASE; Schema: -; Owner: postgres
