ad3a39d53306e4e2605610c3cf9275e9e1ea74ea
SERRA Frank <frank.serra@versusmind.eu>
2019-05-08 18:23:16 +0200

    annulation de la creation des tables par smpring en profil docker

--- sql/medipractice.sql
+++ sql/medipractice.sql
@@ -17,28 +17,6 @@ SET check_function_bodies = false;
 SET client_min_messages = warning;
 SET row_security = off;
 
---
--- TOC entry 2920 (class 1262 OID 16393)
--- Name: medipractice; Type: DATABASE; Schema: -; Owner: postgres
---
-
-CREATE DATABASE medipractice WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'French_France.1252' LC_CTYPE = 'French_France.1252';
-
-
-ALTER DATABASE medipractice OWNER TO postgres;
-
-\connect medipractice
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET client_min_messages = warning;
-SET row_security = off;
-
 SET default_tablespace = '';
 
 SET default_with_oids = false;
